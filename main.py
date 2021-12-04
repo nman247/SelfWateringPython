38520f2b73502d2ae6a406498aea46640cea8de6
nman247 <57647876+nman247@users.noreply.github.com>
2021-12-03 22:31:57 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 import time
 from ADCDevice import *
 from waterpump import *
-
+##Code used from Freenove
 adc = ADCDevice() # Define an ADCDevice class object
 
 def setup():
@@ -22,23 +22,52 @@ def setup():
         "Please use command 'i2cdetect -y 1' to check the I2C address! \n"
         "Program Exit. \n");
         exit(-1)
-        
+##Loop use to detect Voltage from Soil Moisture Meter and turn on WaterPump at a specific level        
 def loop():
     value = adc.analogRead(0)
     print ('ADC Value : ', value)
-    while value >= 40: ##MAKE 80 THE LEVEL
-        value = adc.analogRead(0)    # read the ADC value of channel 0
+    while value >= 80: ##MAKE 80 THE LEVEL
         print ('ADC Value : ', value)
         WaterPump.WaterOn()
+        value = adc.analogRead(0)    # read the ADC value of channel 0
     WaterPump.WaterOff()
     WaterPump.WaterCleanUp()
-    adc.close()
+    
 
 
     
 if __name__ == '__main__':   # Program entrance
     print ('Program is starting ... ')
     setup()
-    loop()
+    ##Sets a CurrentTime
+    currentTime = int(time.localtime().tm_hour)
+    keyinput = "null"; 
+    print("Current Time is: "+str(currentTime))
+    ##Creates a Endless Loop
+    while(keyinput == "null"):
+        ##Sets two times a day for the soil sensor to be used(it is suggested to not used Moisture Sensor continuously)
+        if(currentTime == 12 or currentTime == 24):
+            ##Tells Current Time at Beginning of Loop
+            print("Current Time is: "+str(currentTime))
+            ##Activate the Physical Loop of Project
+            loop()
+        ##Sets a time to ask the user if they want to continue the program
+        ##Best to set at time User is present
+        elif(currentTime == 13):
+            print("A Full Day has Passed!")
+            newkeyInput=input("Press Enter to End the Program (Put in a Input if you want to Continue the Program): ")
+            ##Ends the loop
+            if(newkeyInput == ""):
+                break
+            else:
+                ###Waits a Hour before CurrentTime is checked again
+                time.sleep(3600)
+                currentTime = int(time.localtime().tm_hour)
+                #Continues the Loop
+                continue  
+        currentTime = int(time.localtime().tm_hour)
+         
+    print("Program Ended!")
+    
     
  
--- waterpump.py
+++ waterpump.py
@@ -1,21 +1,28 @@
 ##Created by Nasir Martin
 import RPi.GPIO as GPIO
 import time
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
 class WaterPump:
+    
     def WaterOn():
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
         print("Water Pump On")
         GPIO.setup(20,GPIO.OUT)
         GPIO.setup(20,GPIO.LOW)
     
     def WaterOff():
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
         print("Water Pump Off")
         GPIO.setup(20,GPIO.HIGH)
     def WaterCleanUp():
         GPIO.cleanup()
 
-WaterPump.WaterOff()
+
+
+        
+
+
 
         
 
